c6b4e05aa3ac4768b046490e89d3fd00d7adc32e
xF14x <>
2021-01-31 12:54:35 +0300

    addTheLoginSystem

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from getpass import getpass
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+UserName = input("Pleas Enter Your Name : ")
+Password = getpass("Pleas Enter Your Password : ")
+
+if UserName == "F14Commander" and Password == "1313" :
+    print("welcome")
+else:
+    exit("Error")
