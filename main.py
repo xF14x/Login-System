9b27847a8fc7da5d1b13ece09bb7b44d4306af51
xF14x <>
2021-01-31 12:59:25 +0300

    addSamCommints

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from getpass import getpass
+# Programmit By Suliman Almohawis | TWitter : F14Commander
+
+from getpass import getpass # getpass libery we uset for get The password
+
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-UserName = input("Pleas Enter Your Name : ")
-Password = getpass("Pleas Enter Your Password : ")
+UserName = input("Pleas Enter Your Name : ") # Get UserName
+Password = getpass("Pleas Enter Your Password : ") # Get The Password
 
+# Edit The Username or Password and write Your Username And Password
 if UserName == "F14Commander" and Password == "1313" :
-    print("welcome")
+    print("welcome") # Her i Print Welcome You can write {pass}
 else:
-    exit("Error")
+    exit("Error") # Her i write {exit} for Exit The Program
+
